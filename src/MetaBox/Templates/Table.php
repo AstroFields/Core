5cd5d90fb8d2b1a99bb7d66e5c81f0d2d9bf521d
Kaiser Franz Josef <wecodemore@gmail.com>
2014-07-10 12:51:55 +0200

    Simplified MetaBox template

--- src/MetaBox/Templates/Table.php
+++ src/MetaBox/Templates/Table.php
@@ -3,8 +3,6 @@
 namespace WCM\AstroFields\MetaBox\Templates;
 
 use WCM\AstroFields\Core\Templates\TemplateInterface;
-use WCM\AstroFields\Core\Receivers\DataProviderInterface;
-use WCM\Meta\Commands\MetaBoxInterface;
 
 class Table implements TemplateInterface
 {
@@ -29,19 +27,31 @@ class Table implements TemplateInterface
 	 */
 	public function display()
 	{
-		echo '<table class="wp-list-table  widefat"><tbody>';
-		foreach ( $this->entities as $entity )
-		{
-			$class = 0 === $this->entities->key() %2 ? ' class="alternate"' : '';
-			echo "<tr{$class}>";
-				echo '<td>Foo</td>';
-				echo '<td>';
-					$this->entities
-						->current()
-						->notify();
-				echo '</td>';
-			echo '</tr>';
-		}
-		echo '</tbody></table>';
+		?>
+		<table class="wp-list-table  widefat">
+			<tbody>
+			<?php
+			foreach ( $this->entities as $entity )
+			{
+				$class = 0 === $this->entities->key() %2 ?
+					' class="alternate"'
+					: '';
+				?>
+				<tr<?php echo $class; ?>>
+					<td>Foo</td>
+					<td>
+						<?php
+						$this->entities
+							->current()
+							->notify();
+						?>
+					</td>
+				</tr>
+				<?php
+			}
+			?>
+			</tbody>
+		</table>
+		<?php
 	}
 }
