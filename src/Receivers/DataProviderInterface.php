dbb3b59c0f83563168304e8331a5e6bacb88f951
Kaiser Franz Josef <wecodemore@gmail.com>
2014-11-23 00:49:29 +0100

    docs(php) Extend doc block

--- src/Receivers/DataProviderInterface.php
+++ src/Receivers/DataProviderInterface.php
@@ -2,6 +2,12 @@
 
 namespace WCM\AstroFields\Core\Receivers;
 
+/**
+ * Interface DataProviderInterface
+ * This is the basic recipe for a Receiver that has data attached.
+ * Data can be anything from an ID to something much more meta.
+ * @package WCM\AstroFields\Core\Receivers
+ */
 interface DataProviderInterface
 {
 	public function setData( Array $data );
