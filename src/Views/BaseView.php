5312748d3a411db85aa61762697a82de9b58bb41
Kaiser Franz Josef <wecodemore@gmail.com>
2014-11-23 00:47:23 +0100

    refactor(names) Rename View/Field to View/BaseView

---
+++ src/Views/BaseView.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace WCM\AstroFields\Core\Views;
+
+use WCM\AstroFields\Core\Receivers\FieldInterface;
+use WCM\AstroFields\Core\Templates\TemplateInterface;
+
+class BaseView implements DataAwareInterface, ViewableInterface
+{
+	/** @type Array */
+	private $data;
+
+	/** @type TemplateInterface */
+	private $template;
+
+	public function setTemplate( TemplateInterface $template )
+	{
+		$this->template = $template;
+
+		return $this;
+	}
+
+	public function setData( FieldInterface $data )
+	{
+		$this->data = $data;
+
+		return $this;
+	}
+
+	public function process()
+	{
+		$this->template->attach( $this->data );
+
+		echo $this->template;
+	}
+}
