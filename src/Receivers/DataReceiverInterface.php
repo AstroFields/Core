4126105e52aabd310cdc3b1a16eacde0303a2d19
Kaiser Franz Josef <wecodemore@gmail.com>
2014-11-23 14:07:40 +0100

    feat(interface) Add renamed DataReceiver back in

---
+++ src/Receivers/DataReceiverInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace WCM\AstroFields\Core\Receivers;
+
+/**
+ * Interface DataProviderInterface
+ * This is the basic recipe for a Receiver that has data attached.
+ * Data can be anything from an ID to something much more meta.
+ * @package WCM\AstroFields\Core\Receivers
+ */
+interface DataReceiverInterface
+{
+	public function setData( Array $data );
+}
