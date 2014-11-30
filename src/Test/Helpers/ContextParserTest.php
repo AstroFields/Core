27ca6e329b05a5402f1eb3c254bd8cd993f321ea
Kaiser Franz Josef <wecodemore@gmail.com>
2014-11-30 21:39:31 +0100

    test(context) Add more context situations

--- src/Test/Helpers/ContextParserTest.php
+++ src/Test/Helpers/ContextParserTest.php
@@ -2,16 +2,14 @@
 
 namespace WCM\AstroFields\Core\Test\Helpers;
 
-use WCM\AstroFields\Core\Mediators\Entity;
-use WCM\AstroFields\Core\Helpers\ContextParser;
-
 class ContextParserTest extends \PHPUnit_Framework_TestCase
 {
+	/** @var \WCM\AstroFields\Core\Helpers\ContextParser */
 	private $parser;
 
 	public function setup()
 	{
-		$this->parser = new ContextParser;
+		$this->parser = new \WCM\AstroFields\Core\Helpers\ContextParser;
 	}
 
 	/**
@@ -30,14 +28,12 @@ class ContextParserTest extends \PHPUnit_Framework_TestCase
 	 */
 	public function testResult( $context, Array $input, $expected )
 	{
-		$parser = new ContextParser;
-		$parser->setup( $input, $context );
+		$this->parser->setup( $input, $context );
 
-		$result = $parser->getResult();
+		$result = $this->parser->getResult();
+
 		$this->assertInternalType( 'array', $result );
-		var_dump( $result, $expected );
 		$this->assertEquals( $result, $expected );
-		#$this->assertContains( '', $result );
 	}
 
 	public function getTestContext()
@@ -46,11 +42,36 @@ class ContextParserTest extends \PHPUnit_Framework_TestCase
 			array(
 				'save_post_{type}',
 				array(
-					'{key}'   => array( 'foobar', ),
 					'{type}'  => array( 'post', 'page', ),
 					'{proxy}' => array( 'edit', 'save', ),
 				),
-				array( 'save_post_post', 'save_post_page', ),
+				array(
+					'save_post_post',
+					'save_post_page',
+				),
+			),
+			array(
+				'foo_bar_{key}',
+				array(
+					'{key}' => array( 'baz', 'dragon' ),
+				),
+				array(
+					'foo_bar_baz',
+					'foo_bar_dragon',
+				),
+			),
+			array(
+				'{proxy}_{type}',
+				array(
+					'{proxy}' => array( 'edit', 'save', ),
+					'{type}'  => array( 'post', 'page', ),
+				),
+				array(
+					'edit_post',
+					'edit_page',
+					'save_post',
+					'save_page',
+				),
 			),
 		);
 	}
